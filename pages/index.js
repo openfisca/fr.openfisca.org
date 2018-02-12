f4413ffb45f0b80a044fa16cd78a203f7e0398cb
Anna-Livia <annalivia.gomart@gmail.com>
2018-02-12 15:09:07 +0100

    Créé une première page

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+
+const Index = (props) => (
+  <div>
+      <h1>Openfisca france</h1>
+      <p>le meilleur logiciel au monde</p>
+      <style jsx>{`
+        h1, a {
+          font-family: "Arial";
+        }
+
+        ul {
+          padding: 0;
+        }
+
+        li {
+          list-style: none;
+          margin: 5px 0;
+        }
+
+        a {
+          text-decoration: none;
+          color: blue;
+        }
+
+        a:hover {
+          opacity: 0.6;
+        }
+      `}</style>
+  </div>
+  )
+
+export default Index
