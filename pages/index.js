6e7ccf60b6f1bf68ff32bb625cd64d60f86b6799
Anna-Livia <annalivia.gomart@gmail.com>
2018-03-05 15:14:51 +0100

    Use double quotes for attributes

--- pages/index.js
+++ pages/index.js
@@ -6,32 +6,31 @@ import Footer from "../components/Footer"
 import Hero from "../components/Hero"
 
 const Home = () => (
-  <div id='content'>
-    <div id='outils'>
+  <div id="content">
+    <div id="outils">
       <h2>Les outils à votre disposition</h2>
-      <ul className='flex-container'>
-        <li className='flex-item'>
+      <ul className="flex-container">
+        <li className="flex-item">
           <h3>API Web</h3>
           <p>L'API REST et ses endpoints permet de consulter les données OpenFisca ou d'effectuer des calculs sur la legislation Française.</p>
-          <a className='cta' href='https://legislation.openfisca.fr/swagger'>Interroger</a>
+          <a className="cta" href="https://legislation.openfisca.fr/swagger">Interroger</a>
         </li>
-        <li className='flex-item'>
+        <li className="flex-item">
           <h3>Legislation Explorer</h3>
           <p>Ce site propose toutes les données et calculs disponibles dans OpenFisca avec un moteur de recherche.</p>
-          <a className='cta' href='https://legislation.openfisca.fr'>Explorer</a>
+          <a className="cta" href="https://legislation.openfisca.fr">Explorer</a>
         </li>
-        <li className='flex-item'>
+        <li className="flex-item">
           <h3>Documentation</h3>
           <p>Entièrement en anglais, elle explique comment modéliser un ajout à la législation ou une réforme. </p>
-          <a className='cta' href='https://openfisca.org/doc'>Découvrir</a>
+          <a className="cta" href="https://openfisca.org/doc">Découvrir</a>
         </li>
-        <li className='flex-item'>
+        <li className="flex-item">
           <h3>Code source</h3>
-          <p>L'ensemble du code d'OpenFisca-France peut être lu, forké et clonné depuis la plateforme GitHub.</p>
-          <a className='cta' href='https://github.com/openfisca/openfisca-france'>Le Répo GitHub</a>
+          <p>L'ensemble du code d'OpenFisca-France peut être lu, forké et cloné depuis la plateforme GitHub.</p>
+          <a className="cta" href="https://github.com/openfisca/openfisca-france">Contribuer</a>
         </li> 
       </ul>
-      
     </div>
 
     <div id='commencer'>
@@ -42,13 +41,13 @@ const Home = () => (
           Une API Web
           <p>Pour utiliser l'API Web, nous avons mis en place <a className='reference' href='https://legislation.openfisca.fr/swagger'>une documentation interactive</a>.</p>
         </li>
-        <li className='flex-item'>
+        <li className="flex-item">
           Des Notebooks Juyter
           <p>
             Nous proposons des <a href="https://github.com/openfisca/tutorial/tree/master/notebooks">tutoriels</a>, utilisables dans votre navigateur via <a href="https://mybinder.org/v2/gh/openfisca/tutorial/master?filepath=notebooks/getting_started.ipynb">Binder</a>.
           </p>
         </li>
-        <li className='flex-item'>
+        <li className="flex-item">
           Une API Python
           <p>Après avoir <a href="https://github.com/openfisca/openfisca-france#installation">installé OpenFisca-France</a>, nous proposons <a className="reference" href="https://github.com/openfisca/tutorial">un ensemble de tutoriels</a> en python.</p>
         </li>
@@ -60,26 +59,26 @@ const Home = () => (
     <section>
       <h2>Réutilisations</h2>
       <p>Des services variés font appel à OpenFisca France :</p>
-      <ul className='flex-container'>
-        <li className='flex-item'>
+      <ul className="flex-container">
+        <li className="flex-item">
           <h3>Simulateur Mes Aides</h3>
-          <img alt='Mes Aides' src={asset('/images/mes-aides.png')}/>
-          <a className='reference' href='https://beta.gouv.fr/startup/mes-aides'>Simuler vos aides</a>
+          <img alt='Mes Aides' src={asset("/images/mes-aides.png")}/>
+          <a className="reference" href="https://beta.gouv.fr/startup/mes-aides">Simuler vos aides</a>
         </li>
-        <li className='flex-item'>
+        <li className="flex-item">
           <h3>Portail National des Droits Sociaux</h3>
           <img alt='MFRB' src={asset('/images/logo-pnds.png')}/>
-          <a className='reference' href='https://www.mesdroitssociaux.gouv.fr'>Visiter le portail</a>
+          <a className="reference" href="https://www.mesdroitssociaux.gouv.fr">Visiter le portail</a>
         </li>
-        <li className='flex-item'>
+        <li className="flex-item">
           <h3>Publication du MFRB</h3>
-          <img alt='MFRB' src={asset('/images/logo_mfrb.png')}/>
-          <a className='reference' href='http://www.revenudebase.info/2017/04/07/apprehender-cout-dun-revenu-de-base'>Lire la publication</a>
+          <img alt='MFRB' src={asset("/images/logo_mfrb.png")}/>
+          <a className="reference" href="http://www.revenudebase.info/2017/04/07/apprehender-cout-dun-revenu-de-base">Lire la publication</a>
         </li>
       </ul>
     </section>
 
-    <div id='introduction'>
+    <div id="introduction">
       <h2>Qui sommes nous ?</h2>
       <p>OpenFisca est un moteur ouvert de microsimulation du système socio-fiscal.</p>
       <p>À travers OpenFisca France, une communauté de développeurs, économistes, enseignants, instituts d'études et de recherche
@@ -92,13 +91,13 @@ const Home = () => (
     </div>
     <div>
       <h2>Nos partenaires</h2>
-      <ul className='flex-container'>
-        <li className='flex-item logo'><img alt='IPP' src={asset('/images/logo-ipp.png')} /></li>
-        <li className='flex-item logo'><img alt='Etalab' src={asset('/images/logo-etalab.png')} /></li>
-        <li className='flex-item logo'><img alt='MSA' src={asset('/images/logo-msa.svg')} /></li>
-        <li className='flex-item logo'><img alt='beta.gouv.fr' src={asset('/images/logo-betagouv-url.svg')} /></li>
-        <li className='flex-item logo'><img alt='IDEP' src={asset('/images/logo-idep.png')} /></li>
-        <li className='flex-item logo'><img alt='France Stratégie' src={asset('/images/logo-france-strategie.jpg')} /></li>
+      <ul className="flex-container">
+        <li className="flex-item logo"><img alt='IPP' src={asset('/images/logo-ipp.png')} /></li>
+        <li className="flex-item logo"><img alt='Etalab' src={asset('/images/logo-etalab.png')} /></li>
+        <li className="flex-item logo"><img alt='MSA' src={asset('/images/logo-msa.svg')} /></li>
+        <li className="flex-item logo"><img alt='beta.gouv.fr' src={asset('/images/logo-betagouv-url.svg')} /></li>
+        <li className="flex-item logo"><img alt='IDEP' src={asset('/images/logo-idep.png')} /></li>
+        <li className="flex-item logo"><img alt='France Stratégie' src={asset('/images/logo-france-strategie.jpg')} /></li>
       </ul>
     </div>
 
@@ -196,10 +195,10 @@ const Home = () => (
 
 export default () => (
   <div>
-    <Header page='HomePage'/>
+    <Header page="HomePage"/>
     <Hero/>
     <Home />
     <Footer />
-    <Piwik page='HomePage'/>
+    <Piwik page="HomePage"/>
   </div>
 )
