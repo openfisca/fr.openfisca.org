77e8f01e52e3c4dcde09c9c68596b588dfe93413
sandcha <sandcha@users.noreply.github.com>
2018-04-13 16:34:18 +0200

    Add deployement script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -ex
+pwd
+
+# Update fr.openfisca.org website to last revision
+git checkout master
+git pull
+
+# Rebuild website
+npm install
+npm run build
+
+# The current user must have been specifically allowed to run the next command
+# Use the visudo command to do so
+sudo systemctl restart fr.openfisca.org-beta.service
