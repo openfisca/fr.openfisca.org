f12a0b71f01d3af1ad0644235873bb764cd0b4a3
renardpal <amandine@renardpal.com>
2019-03-15 15:46:31 +0100

    Update resources.js

--- pages/resources.js
+++ pages/resources.js
@@ -5,33 +5,16 @@ import Footer from "../components/Footer"
 import Piwik from "../components/Piwik"
 
 const Resources = () => (
-  <div id='content'>
+  <div className='content'>
     <h1>Ressources</h1>
 
     <style jsx>{`
-      #content{
-        max-width: 42em;
-        margin: auto;
-        padding: 0 3em 1em 3em;
-        font-size: 120%;
-        font-family: "Helvetica Neue",-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-      }
-
       #iframe-container {
         display: flex;
         flex-direction: row;
         justify-content: center;
       }
 
-
-      p {
-        font-weight: 300;
-      }
-
-      a {
-        text-decoration: underline;
-        color: #2b6a8d;
-      }
 
       @media (max-width: 720px) {
         #content {
