db45e595926daa0338aafccd384e7b98c0bcffbf
renardpal <amandine@renardpal.com>
2019-03-15 15:46:29 +0100

    Update start.js

--- pages/start.js
+++ pages/start.js
@@ -1,20 +1,14 @@
-import asset from 'next/asset'
 import GlobalStyle from "../components/GlobalStyle"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
 import Piwik from "../components/Piwik"
 
 const Start = () => (
-  <div id='content'>
+  <div className='content'>
     <h1>Commencer</h1>
 
 
     <style jsx>{`
-      #content{
-        max-width: 42em;
-        margin: auto;
-        padding: 0 3em 1em 3em;
-      }
 
       #iframe-container {
         display: flex;
