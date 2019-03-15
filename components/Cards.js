0d7e44864054c6348f6a5088af70e6aaae011f48
renardpal <amandine@renardpal.com>
2019-03-15 17:49:22 +0100

    Crée cards

---
+++ components/Cards.js
@@ -0,0 +1,12 @@
+import asset from 'next/asset'
+import Link from 'next/link'
+import GlobalStyle from './GlobalStyle'
+
+const Cards = (props) => (
+        <Cards>
+            <titre>headerText</titre>
+
+        </Cards>
+	)
+
+export default Header
