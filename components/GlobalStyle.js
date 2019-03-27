5c71091a21b16fae6949cb4809e340bc780f3adf
renardpal <amandine@renardpal.com>
2019-03-27 09:24:51 +0100

    Update GlobalStyle.js

--- components/GlobalStyle.js
+++ components/GlobalStyle.js
@@ -17,16 +17,73 @@ const GlobalStyle = () => (
         padding: 0 3em 1em 3em;
       }
 
-      @font-face {
-        font-family: "Lato-Regular";
-        src: url("css/fonts/Lato-Regular.otf") format("opentype"),
- 
-        }
+    @font-face {
+        font-family: 'asapbold';
+        src: url('../static/fonts/asap-bold-webfont.woff2') format('woff2'),
+             url('../static/fonts/asap-bold-webfont.woff') format('woff');
+        font-weight: normal;
+        font-style: normal;
+    
+    }
+
+    @font-face {
+        font-family: 'asapbolditalic';
+        src: url('../static/fonts/asap-bolditalic-webfont.woff2') format('woff2'),
+             url('../static/fonts/asap-bolditalic-webfont.woff') format('woff');
+        font-weight: normal;
+        font-style: normal;
+    
+    }
+
+    @font-face {
+        font-family: 'asapitalic';
+        src: url('../static/fonts/asap-italic-webfont.woff2') format('woff2'),
+             url('../static/fonts/asap-italic-webfont.woff') format('woff');
+        font-weight: normal;
+        font-style: normal;
+    
+    }
+    
+    @font-face {
+        font-family: 'asapmedium';
+        src: url('../static/fonts/asap-medium-webfont.woff2') format('woff2'),
+             url('../static/fonts/asap-medium-webfont.woff') format('woff');
+        font-weight: normal;
+        font-style: normal;
+    
+    }
+    
+    @font-face {
+        font-family: 'asapmediumitalic';
+        src: url('../static/fonts/asap-mediumitalic-webfont.woff2') format('woff2'),
+             url('../static/fonts/asap-mediumitalic-webfont.woff') format('woff');
+        font-weight: normal;
+        font-style: normal;
+    
+    }
+
+    @font-face {
+        font-family: 'asapregular';
+        src: url('../static/fonts/asap-regular-webfont.woff2') format('woff2'),
+             url('../static/fonts/asap-regular-webfont.woff') format('woff');
+        font-weight: normal;
+        font-style: normal;
+    
+    }    
+    
+    @font-face {
+        font-family: 'asapsymbol';
+        src: url('../static/fonts/asap-symbol-webfont.woff2') format('woff2'),
+             url('../static/fonts/asap-symbol-webfont.woff') format('woff');
+        font-weight: normal;
+        font-style: normal;
+    
+    }
       
       /* Text styles */
       
       .menu {
-        font-family: Lato;
+        font-family: 'asapregular';
         font-size: 18px;
         font-weight: 500;
         font-style: normal;
@@ -42,7 +99,7 @@ const GlobalStyle = () => (
       }
 
       a {
-        font-family: Lato;
+        font-family: 'asapregular';
         color: #ffffff;
 				display: inline-block;
 				padding: 6px 12px;
@@ -54,131 +111,123 @@ const GlobalStyle = () => (
 			}
    
       h1 {
-        font-family: Lato;
-        font-size: 48px;
-        font-weight: bold;
+        font-family: 'asapbold';
+        font-size: 3em;
         font-style: normal;
         font-stretch: normal;
         line-height: 67.5px;
         letter-spacing: normal;
-        color: #000000;
+        text-align: center;
+        color: #ffffff;
       }
       
       h2 {
-        font-family: Lato;
+        font-family: 'asapmedium';
         font-size: 36px;
-        font-weight: bold;
         font-style: normal;
         font-stretch: normal;
         line-height: 50.6px;
         letter-spacing: normal;
-        color: #000000;
+        color: #424242;
       }
       
       h3 {
-        font-family: Lato;
+        font-family: 'asapmedium';
         font-size: 28px;
-        font-weight: bold;
         font-style: normal;
         font-stretch: normal;
         line-height: 39.4px;
         letter-spacing: normal;
-        color: #000000;
+        color: #424242;
       }
       
       h4 {
-        font-family: Lato;
+        font-family: 'asapmedium';
         font-size: 24px;
-        font-weight: bold;
         font-style: normal;
         font-stretch: normal;
         line-height: 33.8px;
         letter-spacing: normal;
-        color: #000000;
+        color: #424242;
       }
       
       .Large {
-        font-family: Lato;
+        font-family: 'asapregular';
         font-size: 24px;
-        font-weight: normal;
         font-style: normal;
         font-stretch: normal;
         line-height: 36.6px;
         letter-spacing: normal;
-        color: #000000;
+        color: #424242;
       }
       
       .Large-semibold {
-        font-family: Lato;
+        font-family: 'asapmedium';
         font-size: 24px;
-        font-weight: 600;
         font-style: normal;
         font-stretch: normal;
         line-height: 36.6px;
         letter-spacing: normal;
-        color: #000000;
+        color: #424242;
       }
       
       .Medium {
-        font-family: Lato;
+        font-family: asapregular;
         font-size: 20px;
         font-weight: normal;
         font-style: normal;
         font-stretch: normal;
         line-height: 30.5px;
         letter-spacing: normal;
-        color: #000000;
+        color: #424242;
       }
       
       .Medium-SemiBold {
-        font-family: Lato;
+        font-family: asapmedium;
         font-size: 20px;
         font-weight: 600;
         font-style: normal;
         font-stretch: normal;
         line-height: 30.5px;
         letter-spacing: normal;
-        color: #000000;
+        color: #424242;
       }
       
       .Menu {
-        font-family: Lato;
+        font-family: 'asapregular';
         font-size: 18px;
-        font-weight: normal;
         font-style: normal;
         font-stretch: normal;
         line-height: 27.4px;
         letter-spacing: normal;
-        color: #000000;
+        color: #424242;
       }
 
       .Small {
-        font-family: Lato;
+        font-family: 'asapregular';
         font-size: 12px;
-        font-weight: normal;
         font-style: normal;
         font-stretch: normal;
         line-height: 18.3px;
         letter-spacing: normal;
-        color: #000000;
+        color: #424242;
       }
       
       .Regular {
-        font-family: Lato;
+        font-family: 'asapregular';
         font-size: 16px;
-        font-weight: normal;
         font-style: normal;
         font-stretch: normal;
         line-height: 24.4px;
         letter-spacing: normal;
-        color: #000000;
+        color: #424242;
       }
 
       // buttons
       .CTA {
-        -moz-box-shadow: 0px 10px 10px 0px #00000026;
-        -webkit-box-shadow: 0px 10px 10px 0px #00000026;
-        box-shadow: 0px 10px 10px 0px #00000026;
+        -moz-box-shadow: 0px 10px 10px 0px #42424226;
+        -webkit-box-shadow: 0px 10px 10px 0px #42424226;
+        box-shadow: 0px 10px 10px 0px #42424226;
         background-color:#ffffff;
         -moz-border-radius:25px;
         -webkit-border-radius:25px;
@@ -186,7 +235,7 @@ const GlobalStyle = () => (
         display:inline-block;
         cursor:pointer;
         color: #6d69fb;
-        font-family:"LatoBold";
+        font-family:'asapregular';
         font-size:17px;
         padding:10px 10px;
         text-decoration:none;
@@ -210,15 +259,17 @@ const GlobalStyle = () => (
       
       // responsive
 
+      #iframe-container {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+      }
+
       @media (max-width: 720px) {
-				header{
-					flex-direction: column;
-				}
-				ul {
-        padding: 0;
-        flex-direction: column;
-        align-items: center;
-				}
+        #content {
+          padding: 0 1em 1em 1em;
+        }
+        
 			}
     `}</style>
   </div>
