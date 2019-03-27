c2d82badb4a1d8dd9097920eccc06d2c6d427b4b
renardpal <amandine@renardpal.com>
2019-03-27 11:06:11 +0100

    Update mesAides.js

--- pages/mesAides.js
+++ pages/mesAides.js
@@ -2,18 +2,29 @@ import asset from 'next/asset'
 import GlobalStyle from '../components/GlobalStyle'
 import Header from "../components/Header"
 import Footer from "../components/Footer"
+import ProjectDesc from "../components/ProjectDesc"
+import Attributes from "../components/Attributes"
 import Piwik from "../components/Piwik"
 
-const mesaides = () => (
+const Mesaides = () => (
     <div className='content'>
-    
+      <div className='row'>
+      <ProjectDesc img="../static/images/projects/mesaides.png" baseline="MesAides est un simulateur de droit aux aides sociales." desc="Développé par BetaGouv, l’équipe d’innovation du gouvernement français
+Mes Aides informe les citoyens français de leur éligibilité aux aides sociales nationales et régionales. En moins de 7 minutes, le service donne une réponse sur l’éligibilité à 30 aides.
+S’appuie également sur des extensions sur-mesure développées par l’équipe : implémentation d’aides locales (Paris, Brest), création d’outils comme le traceur, outil de debug qui permet de naviguer plus facilement dans un calcul OpenFisca." button="voir le site"/>
+     </div>
+     <div className='row'>
+
+     <Attributes img="../static/images/icons/python.svg" />
+     </div>
+
     </div>
 )
 export default () => (
     <div>
       <GlobalStyle/>
-      <Header page="mesaides" title='Mes Aides' />
-      <mesAides />
+      <Header page='mesaides' title='Mes Aides' />
+      <Mesaides />
       <Footer />
       <Piwik page='mesaides'/>
     </div>
