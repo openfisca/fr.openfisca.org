1a3a5993dd95a2c64c2992cb7c92169a6332813a
renardpal <amandine@renardpal.com>
2019-03-28 18:57:02 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,74 +7,85 @@ import Hero from "../components/Hero"
 
 const Home = () => (
   <div className="content">
-    <section className="what">
+    <section className="who">
       <h2>Tout ce que vous pouvez faire avec OpenFisca !</h2>
-        <li>
-          <ul>
+        <ul className="flex-container">
+          <li className="flex-item3">
             <img src='../static/images/icons/economistes.svg' />
             <h3>Economistes & chercheurs</h3>
-            <p>Utilisez des systèmes socio-fiscaux déjà modélisés et codez vos réformes.
-Faites des simulations individuelles et sur des populations en important vos propres données.</p>
-          </ul>
-          <ul>
+            Utilisez des systèmes socio-fiscaux déjà modélisés et codez vos réformes.
+            Faites des simulations individuelles et sur des populations en important vos propres données.
+          </li>
+          <li className="flex-item3">
             <img src='../static/images/icons/admin.svg' />
             <h3>Administrations publiques</h3>
-            <p>Mutualisez vos efforts de mise à jour grâce au modèle contributif d’OpenFisca. 
-Echangez sur une même base avec d’autres administrations publiques.</p>
-          </ul>
-          <ul>
+            Mutualisez vos efforts de mise à jour grâce au modèle contributif d’OpenFisca. 
+            Echangez sur une même base avec d’autres administrations publiques.
+          </li>
+          <li className="flex-item3">
            <img src='../static/images/icons/dev.svg' />
             <h3>Développeurs & équipes d'innovation</h3>
-            <p>Interrogez l’API sans installation ou hébergez votre propre instance.
-Créez des produits qui interrogent l’ensemble des règles socio-fiscales avec Python3.</p>
-          </ul>
-        </li>
+            Interrogez l’API sans installation ou hébergez votre propre instance.
+            Créez des produits qui interrogent l’ensemble des règles socio-fiscales avec Python3.
+          </li>
+        </ul>
         <a className="btn-medium" href="showcase">Voir des projets</a>
     </section>
+
+
     <section className="how">
       <h2>Comment ça marche ?</h2>
+      <div className="flex-container">
       <p>OpenFisca est un projet Open Source libre et gratuit, modulable et réutilisable. Il permet de modéliser le code législatif en code informatique, pour améliorer la transparence et l’accès à la loi.</p>
 
-      <li>
-        <ul>Utilisez un système socio-fiscal existant</ul>
-        <ul>Codez votre propre système</ul>
-        <ul>Améliorez le code d’un système existant</ul>
-        <a className="btn-medium">Qu'est-ce qu'un système socio-fiscal ?</a>
-      </li>
+      <div className="flex-item2">
+       <img src="../static/images/openlaw.png" />
+     </div>
 
-      <li>
-        <ul>Importez des données</ul>
-        <ul>Calculez une situation ou écrivez et simulez une réforme</ul>
-        <ul>Codez des extensions</ul>
-        <a className="btn-medium" href="http://openfisca.org/doc">Lire la documentation</a>
-      </li>
+      <div className="flex-item2">
 
+        <ul>
+          <li>Utilisez un système socio-fiscal existant</li>
+          <li>Codez votre propre système</li>
+          <li>Améliorez le code d’un système existant</li>
+          <a className="btn-medium">Qu'est-ce qu'un système socio-fiscal ?</a>
+        </ul>
+      </div>
+     </div>
+ 
+     <section className="flex-container">
+        <div className="flex-item2">
+          <ul>
+            <li>Importez des données</li>
+            <li>Calculez une situation ou écrivez et simulez une réforme</li>
+            <li>Codez des extensions</li>
+          </ul>
+          <a className="btn-medium" href="http://openfisca.org/doc">Lire la documentation</a>
+        </div>
+        <div className="flex-item2">
+          <img src="../static/images/api.png" />
+        </div>
+      </section>
     </section>
-    <section className="who">
+
+    <section className="flex-container">
+      <div className="flex-item2">
        <h2>Une communauté open source internationale</h2>
-      <p>OpenFisca a des contributeurs dans 8 pays !
+        <p>OpenFisca a des contributeurs dans 8 pays !
 Rejoignez une communauté d’économistes, de chercheurs, de développeurs et d’experts des droits sociaux qui s’engagent dans la transformation de la loi en code informatique !</p>
+      </div>
+      <div className="flex-item2">
+        <img src="../static/images/community.png" />
+      </div>
     </section>
 
     <style jsx>{`
-
-
-      @media (max-width: 720px) {
-
-        .cta {
-          padding-left: 1em;
-          padding-right: 1em;
-        }
-        .flex-container{
-          flex-direction: column;
-          align-items: center;
-        }
-        .flex-item{
-          width: 100%;
-          min-width: 100%;
-          margin: 1em auto 1em auto
-        }
+      li{
+        list-style: none;
+        padding: 1%;
       }
+
+
     `}</style>
   </div>
   )
