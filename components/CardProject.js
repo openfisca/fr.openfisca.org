77717d4e2f4f7cd69691d1e2e8cad1bfe2163a03
renardpal <amandine@renardpal.com>
2019-03-28 18:55:40 +0100

    Update CardProject.js

--- components/CardProject.js
+++ components/CardProject.js
@@ -5,7 +5,7 @@ import GlobalStyle from './GlobalStyle'
 const CardProject = (props) => (
     <div className="card-project">
         <div className="card-background">
-            <img src={props.img} />
+        <img src={props.img} />
         </div>
         <p className="Medium">{props.title}</p>
         <p className="Small">{props.desc}</p>
@@ -15,18 +15,20 @@ const CardProject = (props) => (
                 background-color: #ffffff;
                 display: flex;
                 flex-flow: column;
-                justify-content: space-between;
-                align-items: center;
+                text-align: left;
+                padding: 0.5em;
                 margin: 1em;
                 max-width: 20%;
                 min-width: 20em;
                 box-shadow: 0px 0px 6px #BCBCC6;
+                border-radius: 4px;
             }
             img{
                 max-width: 100%;
             }
             .card-background {
                 width: 100%;
+                opacity: 0.7;
                 background: #6d69fb; /* Old browsers */
                 background: -moz-linear-gradient(163.99deg, #00BAAF -21.24%, #6D69FB 76.56%, #6D69FB 83.4%); /* FF3.6-15 */
                 background: -webkit-linear-gradient(163.99deg, #00BAAF -21.24%, #6D69FB 76.56%, #6D69FB 83.4%); /* Chrome10-25,Safari5.1-6 */
