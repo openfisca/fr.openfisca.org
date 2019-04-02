08052221e19e11022c1f57b100e97fabe15576b9
renardpal <amandine@renardpal.com>
2019-04-02 18:49:14 +0200

    Add flex thingy

--- pages/community.js
+++ pages/community.js
@@ -7,46 +7,59 @@ import Piwik from "../components/Piwik"
 
 const Communaute = () => (
   <div className='content'>
-
     <div className="flex__container">
      <h2>Qui sommes-nous ?</h2>
-      <div classname="flex-item2">
-        OpenFisca est une communauté Open Source soutenue par Beta Gouv & Etalab, deux équipes d’innovation au sein du gouvernement français.
+      <div classname="card">
+        <p>OpenFisca est une communauté Open Source soutenue par Beta Gouv & Etalab, deux équipes d’innovation au sein du gouvernement français.
         En 2019, OpenFisca est utilisée par 10 pays dans le monde (France, Côte d’Ivoire, Espagne, Italie, Mali, Nouvelle Zélande, Sénégal & Tunisie).
         Plus de 70 personnes ont rejoint cette aventure. Ils contribuent, s’entraident et participent chaque jour à améliorer le code d’OpenFisca.
+        </p>
         <a className="btn medium">Rejoindre le Slack</a>
       </div>
-      <div classname="flex-item2">
-        <img src={asset('/images/community.png')} />
-      </div>
+        <img className="card" src={asset('/images/community.png')} />
     </div>
-
 
     <div className='flex__container'>
      <h2>Echanger la communauté</h2>
-      <div classname="flex-item1">
+      <div classname="flex__item">
       OpenFisca est une communauté Open Source soutenue par Beta Gouv & Etalab, deux équipes d’innovation au sein du gouvernement français.
       En 2019, OpenFisca est utilisée par 10 pays dans le monde (France, Côte d’Ivoire, Espagne, Italie, Mali, Nouvelle Zélande, Sénégal & Tunisie).
       Plus de 70 personnes ont rejoint cette aventure. Ils contribuent, s’entraident et participent chaque jour à améliorer le code d’OpenFisca.
+      </div>
+      <div>
       <a className="btn medium">Voir le GitHub</a>
       </div>
     </div>
 
-    <div className='contact'>
+    <div className='flex__container'>
     <h2>Ecrivez-nous</h2>
     <p>
+      form to come
     </p>
     </div>
-    <div className='news'>
-    <h2>Restez informés</h2>
-      <p>
-    Vous pouvez vous abonner à notre newsletter mensuelle pour recevoir les actualités et les changements majeurs opérés sur OpenFisca.
-      </p>
-      <a className="btn medium">Recevoir la Newsletter</a>
-
+    <div className='flex__container'>
+      <h2>Restez informés</h2>
+        <div classname="flex__item">
+            <div>
+            Vous pouvez vous abonner à notre newsletter mensuelle pour recevoir les actualités et les changements majeurs opérés sur OpenFisca.
+            </div>
+            <a className="btn medium">Recevoir la Newsletter</a>
+        </div>
     </div>
 
     <style jsx>{`
+
+    img {
+      width: 100%;
+      height: auto;
+    }
+
+    .flex__row{
+      display: flex;
+      flex-direction: row;
+      justify-content: flex-start;
+      overflow: hidden;
+    }
 
     `}</style>
   </div>
