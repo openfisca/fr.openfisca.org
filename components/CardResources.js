86614df14e6b8901c009a53cb07af8de4308b1b2
renardpal <amandine@renardpal.com>
2019-04-03 11:52:14 +0200

    Update CardResources.js

--- components/CardResources.js
+++ components/CardResources.js
@@ -1,37 +1,49 @@
 import asset from 'next/asset'
 import Link from 'next/link'
-import GlobalStyle from './GlobalStyle'
 
 const CardResources = (props) => (
-    <div className="card-resource">
-        <div className="card-background">
-            <img className="card-resource" src={props.img} />
+    <div className="card-container">
+        <div className="card-desc">
+            <p className="Medium">{props.title}</p>
+            <img src={props.img} />
+            <p className="Small">
+{props.desc}</p>
         </div>
-        <p className="Medium">{props.title}</p>
-        <p className="Small">{props.desc}</p>
+        <div className="card-attribute">
+            <p>{props.attribute}</p>
+        </div>
 
         <style jsx>{`
-            .card-resource {
+
+            .card-container {
                 background-color: #ffffff;
-                display: flex;
-                flex-flow: column;
-                justify-content: space-between;
-                align-items: center;
-                margin: 1em;
-                max-width: 20%;
-                min-width: 20em;
                 box-shadow: 0px 0px 6px #BCBCC6;
-            }
-            img{
-                max:width: 20%;
-            }
-            .card-background {
+                border-radius: 2%;
+                display: flex;
+                flex-direction: column;
+                text-align: left;
                 width: 100%;
-                background: #6d69fb; /* Old browsers */
-                background: -moz-linear-gradient(163.99deg, #00BAAF -21.24%, #6D69FB 76.56%, #6D69FB 83.4%); /* FF3.6-15 */
-                background: -webkit-linear-gradient(163.99deg, #00BAAF -21.24%, #6D69FB 76.56%, #6D69FB 83.4%); /* Chrome10-25,Safari5.1-6 */
-                background: linear-gradient(163.99deg, #00BAAF -21.24%, #6D69FB 76.56%, #6D69FB 83.4%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
-                filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#6d69fb', endColorstr='#00baaf',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */			
+            }
+
+            .card-desc {
+                text-align: left;
+                padding: 4%;
+            }
+
+            img{
+                color: #6d69fb;
+                width: 40%;
+            }
+
+            .card-desc > p {
+                white-space: normal;
+            }
+
+            .card-attribute {
+                padding: 2%;
+                background-color: #6d69fb;
+                border-radius: 0% 0% 2% 2%;
+            
             }
     `}</style>
       </div>
