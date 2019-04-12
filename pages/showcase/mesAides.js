4670fdcb9f26c1d38c08d08e412d6b47ad732f95
renardpal <amandine@renardpal.com>
2019-04-12 12:58:36 +0200

    Update path

--- pages/showcase/mesAides.js
+++ pages/showcase/mesAides.js
@@ -8,20 +8,19 @@ import Piwik from "../../components/Piwik"
 
 const Mesaides = () => (
     <div className='content'>
-    <a className="outline" href="../showcase" >Retour</a>
-
-      <ProjectDesc img="../static/projects/mesaides.png" baseline="MesAides est un simulateur de droit aux aides sociales." desc1="Développé par BetaGouv, l’équipe d’innovation du gouvernement français"
+    <a className="outline" href="../showcase/" >Retour</a>
+      <ProjectDesc img="/static/projects/mesaides.png" baseline="MesAides est un simulateur de droit aux aides sociales." desc1="Développé par BetaGouv, l’équipe d’innovation du gouvernement français"
 desc2="Mes Aides informe les citoyens français de leur éligibilité aux aides sociales nationales et régionales. En moins de 7 minutes, le service donne une réponse sur l’éligibilité à 30 aides."
 desc3="S’appuie également sur des extensions sur-mesure développées par l’équipe : implémentation d’aides locales (Paris, Brest), création d’outils comme le traceur, outil de debug qui permet de naviguer plus facilement dans un calcul OpenFisca." button="voir le site"/>
 
     <section>
     <h2>Caractéristiques du projet</h2>
       <ProjectAttribute 
-      imgapi="../static/icons/circle/api.svg" api="API Web"
-      imgcountry="../static/icons/circle/nz.svg" country="interroge le système néo-zélandais"
-      imgsimulation="../static/icons/circle/simindividuelle.svg" simulation="Simulation individuelle"
-      imgperimetre="../static/icons/circle/perimetre.svg" perimetre=" Web"
-      imgpoweredby="../static/icons/circle/admin.svg" poweredby="API Web"
+      imgapi="/static/icons/circle/api.svg" api="API Web"
+      imgcountry="/static/icons/circle/nz.svg" country="interroge le système néo-zélandais"
+      imgsimulation="/static/icons/circle/simindividuelle.svg" simulation="Simulation individuelle"
+      imgperimetre="/static/icons/circle/perimetre.svg" perimetre=" Web"
+      imgpoweredby="/static/icons/circle/admin.svg" poweredby="API Web"
       />
   </section>
 
