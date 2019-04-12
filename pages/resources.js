78d529f45cc22717a38774664de358452025a59d
renardpal <amandine@renardpal.com>
2019-04-12 17:56:55 +0200

    Update resources

--- pages/resources.js
+++ pages/resources.js
@@ -4,27 +4,26 @@ import Link from 'next/link'
 import Header from "../components/Header"
 import Footer from "../components/Footer"
 import Piwik from "../components/Piwik"
-import CardResources from "../components/CardResources";
+import CardResources from "../components/CardResources"
+import CardResourcesmin from "../components/CardResourcesmin";
+
 
 const Resources = () => (
   <div className='content'>
     <ul className="flex__container">
-      <li className="flex__item"><CardResources img="/static/icons/france.svg" title="Système : France" desc="Le système contient la majorité des impôts et des prestations sociales. 1567 paramètres - 456 variables." linkexplorer="https://fr.openfisca.org/legislation"  linkgit="https://github.com/openfisca/openfisca-france"/></li>
-      <li className="flex__item"><CardResources img="/static/icons/nz.svg" title="Système : Aotearoa Nouvelle-Zélande" desc="Accédez aux systèmes déjà modélisés ou créez votre propre système socio-fiscal." linkexplorer="https://www.rules.nz"  linkgit="https://github.com/ServiceInnovationLab/openfisca-aotearoa"/></li>
+      <li className="flex__item"><CardResources img="/static/icons/france.svg" title="Système : France" desc="Le système contient la majorité des impôts et des prestations sociales. 45 contributeurs au projet" linkexplorer="https://fr.openfisca.org/legislation"  linkgit="https://github.com/openfisca/openfisca-france"/></li>
+
+      <li className="flex__item"><CardResources img="/static/icons/nz.svg" title="Système : Aotearoa Nouvelle-Zélande" desc="15 contributeurs au projet" linkexplorer="https://www.rules.nz"  linkgit="https://github.com/ServiceInnovationLab/openfisca-aotearoa"/></li>
       
-      <li className="flex__item"><CardResources img="/static/icons/nz.svg" title="Système : Barcelone" desc="Accédez aux systèmes déjà modélisés ou créez votre propre système socio-fiscal." linkexplorer="https://www.rules.nz"  linkgit="https://github.com/ServiceInnovationLab/openfisca-aotearoa"/></li>
+      <li className="flex__item"><CardResourcesmin img="/static/icons/barcelona.svg" title="Système : Barcelone" desc="4 contributeurs au projet" linkgit="https://github.com/lesmevesajudes/openfisca-barcelona"/></li>
 
-      <li className="flex__item"><CardResources img="/static/icons/nz.svg" title="Système : Tunisie" desc="Accédez aux systèmes déjà modélisés ou créez votre propre système socio-fiscal." linkgit="https://github.com/openfisca/openfisca-tunisia"/></li>
+      <li className="flex__item"><CardResourcesmin img="/static/icons/tunisia.svg" title="Système : Tunisie" desc="8 contributeurs au projet" linkgit="https://github.com/openfisca/openfisca-tunisia"/></li>
 
-      <li className="flex__item"><CardResources img="/static/icons/nz.svg" title="Système : Italie" desc="Accédez aux systèmes déjà modélisés ou créez votre propre système socio-fiscal." linkgit="https://github.com/openfisca/openfisca-italy"/></li>
+      <li className="flex__item"><CardResourcesmin img="/static/icons/italy.svg" title="Système : Italie" desc="3 contributeurs au projet" linkgit="https://github.com/openfisca/openfisca-italy"/></li>
 
-      <li className="flex__item"><CardResources img="/static/icons/nz.svg" title="Système : Sénégal" desc="Accédez aux systèmes déjà modélisés ou créez votre propre système socio-fiscal." linkgit="https://github.com/openfisca/openfisca-senegal"/></li>
+      <li className="flex__item"><CardResourcesmin img="/static/icons/senegal.svg" title="Système : Sénégal" desc="13 contributeurs au projet" linkgit="https://github.com/openfisca/openfisca-senegal"/></li>
 
-      <li className="flex__item"><CardResources img="/static/icons/nz.svg" title="Système : Nouvelle-Galle du sud" desc="Accédez aux systèmes déjà modélisés ou créez votre propre système socio-fiscal." linkgit="https://github.com/openfisca/openfisca-italy"/></li>
-
-      <li className="flex__item"><CardResources img="/static/icons/nz.svg" title="Outil : Traceur" desc="Accédez aux systèmes déjà modélisés ou créez votre propre système socio-fiscal." linkgit="https://github.com/openfisca/openfisca-italy"/></li>
-
-      <li className="flex__item"><CardResources img="/static/icons/nz.svg" title="Outil : Swagger" desc="Accédez aux systèmes déjà modélisés ou créez votre propre système socio-fiscal." linkgit="https://github.com/openfisca/openfisca-italy"/></li>
+      <li className="flex__item"><CardResourcesmin img="/static/icons/galle.svg" title="Système : Nouvelle-Galle du sud" desc="4 contributeurs au projet" linkgit="https://github.com/openfisca/openfisca-italy"/></li>
     </ul>
   
     <style jsx>{`
