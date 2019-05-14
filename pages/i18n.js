3665e7a40ef23cf5a8f4aebc9d1e965783f703ac
sandcha <sandcha@users.noreply.github.com>
2019-05-14 15:36:48 +0200

    Add translations lazy loading

--- pages/i18n.js
+++ pages/i18n.js
@@ -1,5 +1,6 @@
 import i18n from "i18next";
 import detector from "i18next-browser-languagedetector";
+import backend from "i18next-xhr-backend";
 import { reactI18nextModule } from "react-i18next";
 
 import indexFR from '../static/language/fr/index.json';
@@ -17,6 +18,7 @@ const resources = {
 
 i18n
   .use(detector)
+  .use(backend)
   .use(reactI18nextModule) // passes i18n down to react-i18next
   .init({
     resources,
