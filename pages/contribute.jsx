69ced6893179991ad5e5e7bf8b4ee93bd8703acc
renardpal <amandine@renardpal.com>
2019-05-20 15:36:57 +0200

    Translate contribute

--- pages/contribute.jsx
+++ pages/contribute.jsx
@@ -14,16 +14,10 @@ const changeLanguage = (lng) => {
 const Contribuer =  withNamespaces()(
   ({t}, props) => (
   <div className="content">
-    <h2>Suggérer ou ajouter des contenus</h2>
-    <h3>Ce site - tout comme ce projet - est contributif !</h3>
-    <p>
-      Vous avez besoin d’un outil ? Il vous manque un élément d’information ? Vous avez une suggestion ?
-      Vous pouvez contribuer via GitHub en créant un outil pour tous - en proposant une modification de contenu ou un ajout.
-      Si vous n’êtes pas en mesure de contribuer, parlez-en la communauté via Slack ou dites-le nous par email !
-      Peut-être que d’autres membres ont le même besoin que vous.
-    </p>
+    <h2>{t('title-suggest')}</h2>
+    <p>{t('content-suggest')}</p>
 
-    <h2>Parler d&#39;OpenFisca</h2>
+    <h2>{t('title-share')}</h2>
     <p>
       Vous souhaitez présenter OpenFisca à quelqu&#39;un ?
       Vous voulez convaincre quelqu&#39;un de l&#39;utiliser ?
@@ -31,12 +25,13 @@ const Contribuer =  withNamespaces()(
       Utilisez notre <a href="https://drive.google.com/open?id=1eGa4PphlDCSzhGSTFDhG6CGRafJi6cAj3vlD7wNw2NM">document de présentation.</a>
     </p>
 
-    <h2>Réutiliser OpenFisca</h2>
+    <h2>{t('title-reuse')}</h2>
     <p>
       OpenFisca utilise la licence AGPL 3.0.
       Merci de citer OpenFisca lors de vos réutilisations !
       Vous pouvez avoir le détail de nos règles de réutilisations sur <a href="https://openfisca.org/doc/publishing.html">la documentation</a>.
     </p>
+
 
     <style jsx>{`
       #iframe-container {
@@ -66,4 +61,5 @@ export default withNamespaces()(({t}) => (
         <Footer />
         <Piwik page="contribute"/>
     </div>
+  )
 )
