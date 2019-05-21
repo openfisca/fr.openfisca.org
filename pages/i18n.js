1478e237d247d7afdb645ffa862f98ca37606d78
renardpal <amandine@renardpal.com>
2019-05-21 15:30:00 +0200

    Import all json into i18n

--- pages/i18n.js
+++ pages/i18n.js
@@ -7,16 +7,57 @@ import indexFR from '../public/locales/fr/index.json';
 import indexEN from '../public/locales/en/index.json';
 import communityFR from '../public/locales/fr/community.json';
 import communityEN from '../public/locales/en/community.json';
+import contributeFR from '../public/locales/fr/contribute.json';
+import contributeEN from '../public/locales/en/contribute.json';
+import legalFR from '../public/locales/fr/legal.json';
+import legalEN from '../public/locales/en/legal.json';
+import resourcesFR from '../public/locales/fr/resources.json';
+import resourcesEN from '../public/locales/en/resources.json';
+import statusFR from '../public/locales/fr/status.json';
+import statusEN from '../public/locales/en/status.json';
+import showcaseFR from '../public/locales/fr/showcase/index.json';
+import showcaseEN from '../public/locales/en/showcase/index.json';
+import fastocheFR from '../public/locales/fr/showcase/fastoche.json';
+import fastocheEN from '../public/locales/en/showcase/fastoche.json';
+import leximpactFR from '../public/locales/fr/showcase/lexImpact.json';
+import leximpactEN from '../public/locales/en/showcase/lexImpact.json';
+import taxippFR from '../public/locales/fr/showcase/taxIpp.json';
+import taxippEN from '../public/locales/en/showcase/taxIpp.json';
+import ratesrebatesFR from '../public/locales/fr/showcase/ratesRebates.json';
+import ratesrebatesEN from '../public/locales/en/showcase/ratesRebates.json';
+import mesaidesFR from '../public/locales/fr/showcase/mesAides.json';
+import mesaidesEN from '../public/locales/en/showcase/mesAides.json';
 
 // the translations
 const resources = {
   en: {
     index: indexEN,
-    community: communityEN
+    community: communityEN,
+    contribute: contributeEN,
+    legal: legalEN,
+    resources: resourcesEN,
+    status: statusEN,
+    showcase: showcaseEN,
+    fastoche: fastocheEN,
+    leximpact: leximpactEN,
+    taxipp: taxippEN,
+    ratesrebates: ratesrebatesEN,
+    mesaides: mesaidesEN    
   },
+
   fr: {
     index: indexFR,
-    community: communityFR
+    community: communityFR,
+    contribute: contributeFR,
+    legal: legalFR,
+    resources: resourcesFR,
+    status: statusFR,
+    showcase: showcaseFR,
+    fastoche: fastocheFR,
+    leximpact: leximpactFR,
+    taxipp: taxippFR,
+    ratesrebates: ratesrebatesFR,
+    mesaides: mesaidesFR    
   }
 };
 
@@ -33,7 +74,7 @@ i18n
   .use(backend)
   .use(reactI18nextModule) // passes i18n down to react-i18next
   .init({
-    ns: ["index", "community"],
+    ns: ["index", "community", "contribute", "cookies", "legal", "resources", "status", "showcase", "fastoche", "leximpact", "mesaides", "ratesrebates", "taxipp"],
     defaultNS: "index",
     backend: {
       // for all available options read the backend's repository readme file
