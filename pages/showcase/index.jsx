6bb2107cc6f1586e9f6088140cb17d8f7427a3b3
sandcha <sandcha@users.noreply.github.com>
2019-05-22 19:54:46 +0200

    Fix missing translation key in showcase

--- pages/showcase/index.jsx
+++ pages/showcase/index.jsx
@@ -13,37 +13,32 @@ const Showcase =  withNamespaces("showcase")(
     <ul className="flex__container">
       <li className="flex__item">
         <a href="mesAides" >
-          <CardProject img="/static/projects/mesaides.png" title={t('title-mesaides')} 
-            desc={t('desc-mesaides')} 
-            attribute1={t('France')} attribute2={t('apiweb')} attribute3={t('social')} />
+          <CardProject img="/static/projects/mesaides.png" title={t('title-mesaides')} desc={t('desc-mesaides')} 
+            attribute1={t('fr')} attribute2={t('apiweb')} attribute3={t('social')} />
         </a>
       </li>
       <li className="flex__item">
         <a href="lexImpact" >
-          <CardProject img="/static/projects/leximpact.png" title={t('title-lex')} 
-            desc={t('desc-lex')} 
-            attribute1={t('France')} attribute2={t('apipython')} attribute3={t('reforms')} />
+          <CardProject img="/static/projects/leximpact.png" title={t('title-lex')} desc={t('desc-lex')} 
+            attribute1={t('fr')} attribute2={t('apipython')} attribute3={t('reforms')} />
         </a>
       </li>
       <li className="flex__item">
         <a href="taxIpp" >
-          <CardProject img="/static/projects/taxipp.png" title={t('title-tax')} 
-            desc={t('desc-tax')} 
-            attribute1={t('France')} attribute2={t('apipython')} attribute3={t('reforms')} />
+          <CardProject img="/static/projects/taxipp.png" title={t('title-tax')} desc={t('desc-tax')} 
+            attribute1={t('fr')} attribute2={t('apipython')} attribute3={t('reforms')} />
         </a>
       </li>
       <li className="flex__item">
         <a href="ratesRebates" >
-          <CardProject img="/static/projects/ratesrebates.png" title={t('title-rates')} 
-            desc={t('desc-rates')} 
+          <CardProject img="/static/projects/ratesrebates.png" title={t('title-rates')} desc={t('desc-rates')} 
             attribute1={t('nz')} attribute2={t('apiweb')} attribute3={t('social')} />
         </a>
       </li>
       <li className="flex__item">
         <a href="fastoche" >
-          <CardProject img="/static/projects/fastoche.png" title={t('title-fastoche')} 
-            desc={t('desc-fastoche')} 
-            attribute1={t('France')} attribute2={t('apiweb')} attribute3={t('social')} />
+          <CardProject img="/static/projects/fastoche.png" title={t('title-fastoche')} desc={t('desc-fastoche')} 
+            attribute1={t('fr')} attribute2={t('apiweb')} attribute3={t('social')} />
         </a>
       </li>
     </ul>
@@ -73,11 +68,11 @@ const Showcase =  withNamespaces("showcase")(
 export default withNamespaces("showcase")(({t}) => (
     <div>
         <Head>
-          <title>{t('desc-fr')}</title>
+          <title>{t('title')}</title>
           <meta name="viewport" key="viewport" content="initial-scale=1.0, width=device-width" />
         </Head>
         <GlobalStyle/>
-        <Header page="showcase" title={t('showcase:title')} />
+        <Header page="showcase" title={t('title')} />
         <Showcase />
         <Footer />
         <Piwik page="showcase"/>
